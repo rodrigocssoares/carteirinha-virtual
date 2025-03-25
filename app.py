950a12259d2295f29e64e02a80b16a18027c7d6c
rodrigocssoares <rod1234@gmail.com>
2025-03-24 22:15:28 -0300

    ajusta margem texto 40

--- app.py
+++ app.py
@@ -27,7 +27,10 @@ def generate_card(img_path, nome, estado, cidade, categoria, cor_texto, result_f
         font = ImageFont.load_default()
 
     texto = f"Atleta: {nome}\nEstado: {estado}\nCidade: {cidade}\nCategoria: {categoria}"
-    draw.text((40, base.height - 100), texto, fill=cor_texto, font=font)
+    text_width, text_height = draw.multiline_textbbox((0, 0), texto, font=font, spacing=4)[2:]
+    text_position = (40, base.height - text_height - 40)
+
+    draw.multiline_text(text_position, texto, fill=cor_texto, font=font, spacing=4)
 
     result_path = os.path.join(app.config['RESULT_FOLDER'], result_filename)
     base.save(result_path)
@@ -37,7 +40,8 @@ def generate_card(img_path, nome, estado, cidade, categoria, cor_texto, result_f
 def index():
     imagem_gerada = None
     nome = estado = cidade = categoria = cor = ''
-    img_path = None
+    result_path = ''
+    img_path = OVERLAY_PATH
 
     if request.method == 'POST':
         nome = request.form.get('nome', '')
@@ -47,7 +51,9 @@ def index():
         cor = request.form.get('cor', '#FFFFFF')
         acao = request.form.get('acao')
 
-        img_path = request.form.get('imagem_path', '')
+        imagem_path = request.form.get("imagem_path", "")
+        if imagem_path and os.path.exists(imagem_path):
+            img_path = imagem_path
 
         if 'imagem' in request.files:
             imagem = request.files['imagem']
@@ -56,17 +62,16 @@ def index():
                 img_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
                 imagem.save(img_path)
 
-        if img_path:
-            result_filename = f"final_{secure_filename(nome)}.png"
-            result_path = generate_card(img_path, nome, estado, cidade, categoria, cor, result_filename)
+        result_filename = f"final_{secure_filename(nome)}.png"
+        result_path = generate_card(img_path, nome, estado, cidade, categoria, cor, result_filename)
 
-            if acao == 'visualizar':
-                imagem_gerada = result_path
-            elif acao == 'baixar':
-                return send_file(result_path, as_attachment=True)
+        if acao == 'visualizar':
+            imagem_gerada = result_path
+        elif acao == 'baixar':
+            return send_file(result_path, as_attachment=True)
 
     return render_template(
-        'index.html',
+        "index.html",
         nome=nome,
         estado=estado,
         cidade=cidade,
@@ -75,3 +80,6 @@ def index():
         imagem_gerada=imagem_gerada,
         imagem_path=img_path
     )
+
+if __name__ == "__main__":
+    app.run(debug=True)
